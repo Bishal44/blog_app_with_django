d16dacf7a9d912b3f247c18c72bd6ede51946084
Bishal bhattarai <bhattaraibishal704@gmail.com>
2019-09-14 13:16:09 +0545

    Update views.py

--- src/blog/api/views.py
+++ src/blog/api/views.py
@@ -3,9 +3,3 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from  blog.models import BlogPost
 from blog.api.serializers import BlogPostSerializer
-# @api_view(['GET',])
-# def api_detail_blog(request,sl):
-#     try:
-#         blogpost=BlogPost.objects.get(sl)
-#     except BlogPost.DoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
